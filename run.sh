77041497e315754cf4aead59874879c618efc1f6
Cesare Montresor <cesare.montresor@gmail.com>
2022-11-02 16:37:08 +0100

    - lesson 6 but doesn't converge

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 #conda init bash
 #conda activate ai-Lab
-jt -t monokai -f roboto -fs 9 -cellw 100% -T
+jt -t monokai -T -cellw 100% -m 0 -f roboto -fs 9 -nfs 9 -tfs 9 -dfs 9 -ofs 9 -mathfs 9
 jupyter notebook
