75c7237eea55866d3dca20c26accfd95487cabed
Cesare Montresor <cesare.montresor@gmail.com>
2022-11-17 22:03:36 +0100

    - recovered lesson 3 - be - added comments

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-#! /bin/sh
+#! /bin/bash
 
 #conda init bash
 #conda activate ai-Lab
 jt -t monokai -T -cellw 100% -m 0 -f roboto -fs 9 -nfs 9 -tfs 9 -dfs 9 -ofs 9 -mathfs 9
-jupyter notebook
+jupyter notebook&
